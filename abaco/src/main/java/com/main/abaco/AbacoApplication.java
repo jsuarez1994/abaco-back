4e62e4dd387320f6a7ba84a9f37b1ce97ecbf3ed
Jesus Suarez <jesus1994sl@gmail.com>
2020-11-03 23:03:09 +0100

    First Commit

---
+++ abaco/src/main/java/com/main/abaco/AbacoApplication.java
@@ -0,0 +1,13 @@
+package com.main.abaco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbacoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbacoApplication.class, args);
+	}
+
+}
